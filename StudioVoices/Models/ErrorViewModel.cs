cc3480c63041296c7580f15cc148750a1c50e435
Susan van Praag <susan@infi.nl>
2019-04-17 13:12:17 +0200

    Add project files.

---
+++ StudioVoices/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudioVoices.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
