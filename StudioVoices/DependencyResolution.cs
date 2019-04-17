39a07759f271c4a50b68596396d9bcb49c09752f
Susan van Praag <susan@infi.nl>
2019-04-17 16:55:39 +0200

    add dependency resolution and connectionstring to appsettings

---
+++ StudioVoices/DependencyResolution.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace StudioVoices
+{
+    public static class DependencyResolution
+    {
+        public static IServiceCollection AddDependencyResolution(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
--- StudioVoices/Startup.cs
+++ StudioVoices/Startup.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StudioVoices.Data;
@@ -35,14 +30,10 @@ namespace StudioVoices
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddDefaultUI(UIFramework.Bootstrap4)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>().AddDefaultUI(UIFramework.Bootstrap4).AddEntityFrameworkStores<ApplicationDbContext>(); //todo: connectie met authDB toevoegen
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddDependencyResolution();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
